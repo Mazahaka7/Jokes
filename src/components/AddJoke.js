3409c163c820db38b8e7dd0108bc69215b9aa5c5
git <dima.it.ua@gmail.com>
2023-11-21 08:02:41 +0200

    little fix

--- src/components/AddJoke.js
+++ src/components/AddJoke.js
@@ -20,9 +20,7 @@ function AddJoke(props) {
 
   return (
     <form onSubmit={submitHandler}>
-      <h1 className={styles.heading}>
-        Make the world more smiling - add your joke
-      </h1>
+      <h1 className={styles.heading}>Make the world smiling - add joke</h1>
       <div className={styles.control}>
         <label htmlFor="type">Type</label>
         <input type="text" id="type" ref={typeRef}></input>
@@ -32,7 +30,7 @@ function AddJoke(props) {
         <textarea rows={5} type="text" id="setup" ref={setupRef}></textarea>
       </div>
       <div className={styles.control}>
-        <label htmlFor="punchline">Punchline</label>
+        <label htmlFor="punchline">Summary</label>
         <textarea
           rows={5}
           type="text"
